f08256033cd9370b6a73732dcdaaf581a342e1fa
s-traut <stephane.traut@gmail.com>
2022-01-24 12:26:36 +0100

    fixing easing export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { Storyboard } from './src/storyboard.js';
 export { Sprite } from './src/sprite.js';
 export { newEvent } from './src/event.js';
-export { Easing } from './src/easing.js';
+export { ease } from './src/easing.js';
