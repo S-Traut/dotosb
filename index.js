f6a9b0e13fab4dac94c66de614bea9fd21d6e12e
s-traut <stephane.traut@gmail.com>
2022-03-21 23:44:56 +0100

    Clean up

--- index.js
+++
@@ -1,4 +0,0 @@
-export { Storyboard, fromFile } from './src/storyboard.js';
-export { Sprite } from './src/sprite.js';
-export { newEvent } from './src/event.js';
-export { ease } from './src/easing.js';
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { Storyboard, fromFile } from './storyboard.js';
+export { Sprite } from './sprite.js';
+export { newEvent } from './event.js';
+export { ease } from './easing.js';
