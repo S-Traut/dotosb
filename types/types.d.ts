d377b5639fae5025548fb4551f4b5b26758cb74a
s-traut <stephane.traut@gmail.com>
2022-03-26 11:01:07 +0100

    Updating types

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,41 +1,103 @@
 declare module "dotosb" {
-
   class Storyboard {
     private layers: Map<string, Array<Sprite>>;
 
     createSprite(path: string, options?: Partial<SpriteOptions>): Sprite;
     write(file_path: string): void;
+    addSprite(sprite: Sprite): void;
     toString(): string;
   }
 
-  function fromFile(file_path: string): Storyboard;
+  enum Easing {
+    Linear,
+    Out,
+    In,
+    Quad,
+    QuadOut,
+    QuadInOut,
+    CubicIn,
+    CubicOut,
+    CubicInOut,
+    QuartIn,
+    QuartOut,
+    QuartInOut,
+    QuintIn,
+    QuintOut,
+    QuintInOut,
+    SineIn,
+    SineOut,
+    SineInOut,
+    ExpoIn,
+    ExpoOut,
+    ExpoInOut,
+    CircIn,
+    CircOut,
+    CircInOut,
+    ElasticIn,
+    ElasticOut,
+    ElasticHalfOut,
+    ElasticQuarterOut,
+    ElasticInOut,
+    BackIn,
+    BackOut,
+    BackInOut,
+    BounceIn,
+    BounceOut,
+    BounceInOut,
+  }
 
-    class Sprite {
+  enum Event {
+    Fade = "F",
+    Scale = "S",
+    Rotate = "R",
+    Move = "M",
+    MoveX = "MX",
+    MoveY = "MY",
+    Color = "C",
+    vecScale = "V",
+  }
+
+  function fromFile(file_path: string): Storyboard;
+  function ease(easing: Easing, value: number): number;
+
+  class Sprite {
+    private events: [];
+    private path: string;
+    private origin: string;
+    private layer: string;
+    private x: number;
+    private y: number;
+
     constructor(path: string, options?: Partial<SpriteOptions>);
 
     add(
       type: string,
-      times: Array<number>,
-      values: Array<number>,
+      times: Array<number> | number,
+      values: Array<number> | number,
       easing?: number
     ): void;
+
     param(start: number, end: number, type: string): void;
+
     createLoop(
       start: number,
       loop_count: number,
       events: Array<OsbEvent | OsbParameter>
     ): void;
+
+    getAt(type: Event, time: number): number | number[];
+
     toString(): string;
   }
 
   type SpriteOptions = {
-    layer: String;
-    origin: String;
-    x: Number;
-    y: Number;
+    layer?: String;
+    origin?: String;
+    x?: Number;
+    y?: Number;
   };
-  
-    type OsbEvent = {
+
+  type OsbEvent = {
     type: string;
     easing: number;
     start: number;
@@ -54,8 +116,8 @@ declare module "dotosb" {
 
   function newEvent(
     type: string,
-    times: Array<number>,
-    values: Array<number>,
+    times: Array<number> | number,
+    values: Array<number> | number,
     easing: number
   ): OsbEvent;
   function newParam(start: number, end: number, type: string): OsbParameter;
