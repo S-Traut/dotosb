3f268d6d65fee4580d9fd669861694771dfadb27
s-traut <stephane.traut@gmail.com>
2022-03-26 11:01:13 +0100

    Updating exports

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const Sprite = require('./src/sprite.js');
 const Animation = require('./src/animation');
 const Events = require('./src/event.js');
 const Easings = require('./src/easing.js');
+const { EASINGS, EVENTS } = require('./src/utils.js');
 
 exports.Storyboard = Storyboard.Storyboard;
 exports.Sprite = Sprite.Sprite;
 exports.Animation = Animation.Animation;
 exports.createEvent = Events.newEvent;
 exports.createParam = Events.newParam;
-exports.easings = Easings;
+exports.Easing = EASINGS;
+exports.Event = EVENTS;
+exports.ease = Easings.ease;
